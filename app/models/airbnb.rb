325ed5fabe54e2db45251e40947346173f0a48f2
Anthony Ellis <ajellis1989@gmail.com>
2015-09-24 16:52:43 -0400

    Adds response methods for airbnb and event models

--- app/models/airbnb.rb
+++ app/models/airbnb.rb
@@ -1,2 +1,19 @@
+require 'httparty'
 class Airbnb
+
+   def initialize
+     @response = get_response
+   end
+
+   private def get_response
+     key = ENV['ZILYO_KEY']
+     HTTParty.get("https://zilyo.p.mashage.com/search",  initheader = {
+       "X-Mashape-Key"=>"#{key}",
+       "Accept" => "application/json"
+     })
+   end
+
+
+
+
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,14 @@
+require 'httparty'
 class Event
+
+  def initialize
+    @response = get_response
+  end
+
+  private def get_response
+    key = ENV['EVENTBRITE_KEY']
+    HTTParty.get("https://www.eventbrite.com/oauth/authorize?response_type=code&client_id=#{key}")
+  end
+
+
 end
