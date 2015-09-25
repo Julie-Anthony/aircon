833f1c41c7f03c2e6c9929b27e5b74908aeff8be
Anthony Ellis <ajellis1989@gmail.com>
2015-09-25 11:47:27 -0400

    Adds venue id, latitude, and longitude to get_location method

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,6 +5,8 @@ class Event
     @response_search = get_response_search
     @response_venue_id = get_response_venue_location
     @search_params = search_params
+    @latitude = latitude
+    @longitude = longitude
   end
 
   def search
@@ -15,18 +17,22 @@ class Event
   end
 
   def get_location
+    venue_id = @response_search["events"][0]["venue_id"]
+    latitude = @response_venue_id["address"]["latitude"].to_i
+    longitude = @response_venue_id["address"]["longitude"].to_i
+
     # This is where we will use @response_search to get the venue_id and then we will use
     # @response_venue_id to use the venue_id to get to venue page to find the latitude and longitude.
   end
 
   private def get_response_search
-    key = ENV['EVENTBRITE_KEY']
-    HTTParty.get("https://www.eventbriteapi.com/v3/events/search/?token=#{key}#{@search_params}")
+    anon_key = ENV['EVENTBRITE_ANON_KEY']
+    HTTParty.get("https://www.eventbriteapi.com/v3/events/search/?token=#{anon_key}#{@search_params}")
   end
 
   private def get_response_venue_location
-    anon_key = ENV['EVENTBRITE_ANON_KEY']
-    HTTParty.get("https://www.eventbriteapi.com/v3/venues/#{venue_id}/?token=#{anon_key}")
+      personal_key = ENV['EVENTBRITE_PERSONAL_KEY']
+    HTTParty.get("https://www.eventbriteapi.com/v3/venues/#{venue_id}/?token=#{personal_key}")
   end
   # We are parsing two different pages, so we need two different instance variables.
 end
