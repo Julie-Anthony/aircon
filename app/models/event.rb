3f17032878629f9c9d9ebf2bb76fe0741bcbf391
Anthony Ellis <ajellis1989@gmail.com>
2015-09-25 11:14:03 -0400

    Adds private search and venue location methods to events models"

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,13 +2,31 @@ require 'httparty'
 class Event
 
   def initialize
-    @response = get_response
+    @response_search = get_response_search
+    @response_venue_id = get_response_venue_location
+    @search_params = search_params
   end
 
-  private def get_response
+  def search
+    puts "Enter search query for your event: "
+    input = gets.chomp
+    @search_params = "&q=#{input}"
+
+  end
+
+  def get_location
+    # This is where we will use @response_search to get the venue_id and then we will use
+    # @response_venue_id to use the venue_id to get to venue page to find the latitude and longitude.
+  end
+
+  private def get_response_search
     key = ENV['EVENTBRITE_KEY']
-    HTTParty.get("https://www.eventbrite.com/oauth/authorize?response_type=code&client_id=#{key}")
+    HTTParty.get("https://www.eventbriteapi.com/v3/events/search/?token=#{key}#{@search_params}")
   end
 
-
+  private def get_response_venue_location
+    anon_key = ENV['EVENTBRITE_ANON_KEY']
+    HTTParty.get("https://www.eventbriteapi.com/v3/venues/#{venue_id}/?token=#{anon_key}")
+  end
+  # We are parsing two different pages, so we need two different instance variables.
 end
